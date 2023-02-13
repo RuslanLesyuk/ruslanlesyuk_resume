d15b5fbd22ce3dc4a047cf92f0b581caae978560
oWiCo <lesyukruslan@gmail.com>
2023-02-13 13:44:40 +0200

    updated

---
+++ docker/volume/mysql/EXP_Dump.sql
@@ -0,0 +1,203 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
+--
+-- Host: localhost    Database: EXP
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Ansible`
+--
+
+DROP TABLE IF EXISTS `Ansible`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Ansible` (
+  `description` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Ansible`
+--
+
+LOCK TABLES `Ansible` WRITE;
+/*!40000 ALTER TABLE `Ansible` DISABLE KEYS */;
+INSERT INTO `Ansible` VALUES ('Ansible(Basic)');
+/*!40000 ALTER TABLE `Ansible` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `CSS`
+--
+
+DROP TABLE IF EXISTS `CSS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `CSS` (
+  `description` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `CSS`
+--
+
+LOCK TABLES `CSS` WRITE;
+/*!40000 ALTER TABLE `CSS` DISABLE KEYS */;
+INSERT INTO `CSS` VALUES ('CSS(Basic)');
+/*!40000 ALTER TABLE `CSS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Docker`
+--
+
+DROP TABLE IF EXISTS `Docker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Docker` (
+  `description` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Docker`
+--
+
+LOCK TABLES `Docker` WRITE;
+/*!40000 ALTER TABLE `Docker` DISABLE KEYS */;
+INSERT INTO `Docker` VALUES ('Docker-Compose(Basic)');
+/*!40000 ALTER TABLE `Docker` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `GitHub`
+--
+
+DROP TABLE IF EXISTS `GitHub`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `GitHub` (
+  `description` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `GitHub`
+--
+
+LOCK TABLES `GitHub` WRITE;
+/*!40000 ALTER TABLE `GitHub` DISABLE KEYS */;
+INSERT INTO `GitHub` VALUES ('GitHub(Basic)');
+/*!40000 ALTER TABLE `GitHub` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `HTML`
+--
+
+DROP TABLE IF EXISTS `HTML`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `HTML` (
+  `description` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `HTML`
+--
+
+LOCK TABLES `HTML` WRITE;
+/*!40000 ALTER TABLE `HTML` DISABLE KEYS */;
+INSERT INTO `HTML` VALUES ('HTML(Basic)');
+/*!40000 ALTER TABLE `HTML` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Jenkins`
+--
+
+DROP TABLE IF EXISTS `Jenkins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Jenkins` (
+  `description` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Jenkins`
+--
+
+LOCK TABLES `Jenkins` WRITE;
+/*!40000 ALTER TABLE `Jenkins` DISABLE KEYS */;
+INSERT INTO `Jenkins` VALUES ('Jenkins(Basic)');
+/*!40000 ALTER TABLE `Jenkins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Linux`
+--
+
+DROP TABLE IF EXISTS `Linux`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Linux` (
+  `description` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Linux`
+--
+
+LOCK TABLES `Linux` WRITE;
+/*!40000 ALTER TABLE `Linux` DISABLE KEYS */;
+INSERT INTO `Linux` VALUES ('Linux(Basic)');
+/*!40000 ALTER TABLE `Linux` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `MySQL`
+--
+
+DROP TABLE IF EXISTS `MySQL`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `MySQL` (
+  `description` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `MySQL`
+--
+
+LOCK TABLES `MySQL` WRITE;
+/*!40000 ALTER TABLE `MySQL` DISABLE KEYS */;
+INSERT INTO `MySQL` VALUES ('MySQL(Basic)');
+/*!40000 ALTER TABLE `MySQL` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-02-13 11:06:27
---
+++ src/index.php
@@ -0,0 +1,238 @@
+<!DOCTYPE HTML>
+<?php  
+$host = 'mysql';  
+$user = 'root';  
+$pass = 'root';  
+$dbname = 'EXP';  
+$conn = mysqli_connect($host, $user, $pass,$dbname);  
+if(!$conn){  
+  die('Could not connect: '.mysqli_connect_error());  
+}  
+
+?>
+<html>
+<head>
+<meta meta="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<title>Ruslan Lesyuk</title>
+</head>
+<body>
+  <div class="container">
+    <div class="left_side">
+      <div class="profileText">
+        <div class="imgBx">
+          <img src="/img/IMG_0097.JPG">
+          </div>
+          <h2>Ruslan Lesyuk<br><span>DevOps</span></h2>
+      </div>
+
+      <div class="contactInfo">
+        <h3 class="title">Contact Info<h3>
+        <ul>
+          <li>
+            <span class="icon"><i class="fa fa-phone" aria-hidden="true"></i></span>
+            <span class="text">+38 067 351 56 36</span>
+          </li>
+          <li>
+            <span class="icon"><i class="fa fa-envelope" aria-hidden="true"></i>
+</span>
+            <span class="text">lesyukruslan@gmail.com</span>
+          </li>
+          <li>
+            <span class="icon"><i class="fa fa-globe" aria-hidden="true"></i>
+</span>
+            <span class="text">www.localhost.com</span>
+          </li>
+          <li>
+            <span class="icon"><i class="fa fa-linkedin" aria-hidden="true"></i>
+</span>
+            <a href="https://www.linkedin.com/in/ruslan-lesiuk-019901255/"><span class="text" >www.linkedin.com</span></a>
+          </li>
+          <li>
+            <span class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i>
+</span>
+            <span class="text">Ternopil, Ukraine</span>
+          </li>
+        </ul>
+      </div>
+
+      <div class="contactInfo education">
+        <h3 class="title">Education<h3>
+        <ul>
+          <li>
+            <h5>2022 - 2023</h5>
+            <h4>Student</h4>
+            <h4>IT Academy "ITEA"
+          </li>
+        </ul>
+      </div>
+
+      <div class="contactInfo language">
+        <h3 class="title">Languages<h3>
+        <ul>
+          <li>
+            <span class="text">English(B1)</span>
+            <span class="percent">
+               <div style="width: 50%;"></div>
+            </span>
+              
+          </li>
+          <li>
+            <span class="text">Ukrainian(Native)</span>
+            <span class="percent">
+            <div style="width: 100%;"></div>
+            </span>
+          </ul>
+      </div>
+    </div>
+  <div class="righ_side">
+    
+  
+  <div class="about">
+      <h2 class="title2">Profile</h2>
+      <p><pre>Position:  Intern DevOps<pre>
+      <p><pre>Birthday:  02.01.1990 р.<pre>
+			<p>Worldwide contract support as a private entrepreneur
+      <p>Open for new oppurtunities
+      
+    </div>
+    
+    <div class="about">
+      <h2 class="title2">Experience</h2>
+    <div class="box">
+      <div class="year_company">
+    <h5>2023 - </h5>
+    <h5>Company Name</h5>
+    </div>
+</div>
+</div>
+    <div class="about">
+      <h2 class="title2">Skills</h2>
+    <div class="box">
+      <div class="year_company">
+    <h5><?php
+    $sql = 'SELECT description FROM Docker';  
+$retval=mysqli_query($conn, $sql);  
+  
+if(mysqli_num_rows($retval) > 0){  
+ while($row = mysqli_fetch_assoc($retval)){  
+    echo "{$row['description']}";  
+           
+ } //end of while  
+}else{  
+echo "0 results";  
+}  
+ 
+?></h5>
+    <h5><?php
+    $sql = 'SELECT description FROM GitHub';  
+$retval=mysqli_query($conn, $sql);  
+  
+if(mysqli_num_rows($retval) > 0){  
+ while($row = mysqli_fetch_assoc($retval)){  
+    echo "{$row['description']}";  
+           
+ } //end of while  
+}else{  
+echo "0 results";  
+}  
+  
+?></h5>
+    <h5><?php
+    $sql = 'SELECT description FROM MySQL';  
+$retval=mysqli_query($conn, $sql);  
+  
+if(mysqli_num_rows($retval) > 0){  
+ while($row = mysqli_fetch_assoc($retval)){  
+    echo "{$row['description']}";  
+           
+ } //end of while  
+}else{  
+echo "0 results";  
+}  
+  
+?></h5>
+    <h5><?php
+    $sql = 'SELECT description FROM Ansible';  
+$retval=mysqli_query($conn, $sql);  
+  
+if(mysqli_num_rows($retval) > 0){  
+ while($row = mysqli_fetch_assoc($retval)){  
+    echo "{$row['description']}";  
+           
+ } //end of while  
+}else{  
+echo "0 results";  
+}  
+  
+?></h5>
+    <h5><?php
+    $sql = 'SELECT description FROM Linux';  
+$retval=mysqli_query($conn, $sql);  
+  
+if(mysqli_num_rows($retval) > 0){  
+ while($row = mysqli_fetch_assoc($retval)){  
+    echo "{$row['description']}";  
+           
+ } //end of while  
+}else{  
+echo "0 results";  
+}  
+ 
+?>
+    <h5><?php
+    $sql = 'SELECT description FROM HTML';  
+$retval=mysqli_query($conn, $sql);  
+  
+if(mysqli_num_rows($retval) > 0){  
+ while($row = mysqli_fetch_assoc($retval)){  
+    echo "{$row['description']}";  
+           
+ } //end of while  
+}else{  
+echo "0 results";  
+}  
+  
+?></h5>
+    <h5><?php
+    $sql = 'SELECT description FROM CSS';  
+$retval=mysqli_query($conn, $sql);  
+  
+if(mysqli_num_rows($retval) > 0){  
+ while($row = mysqli_fetch_assoc($retval)){  
+    echo "{$row['description']}";  
+           
+ } //end of while  
+}else{  
+echo "0 results";  
+}  
+mysqli_close($conn);  
+?> </h5>
+    </div>
+    
+  
+  </div>
+  </div>
+  <div class="about">
+      <h2 class="title2">Summary</h2>
+      <p>I have no experience in the IT field, 
+         but I would really like to get it, I graduated from the IT school "ITEA", DevOps, I have a great desire to work and self-realize new skills, I am always active and have the ability to self-study. 
+  </div>
+  <div class="about">
+      <h2 class="title2">Quote</h2>
+      <p>No one can know everything, but they can always get closer, my motto is to work for self-improvement.
+  </div>
+  <div class="Hobbies">
+      <h2 class="title2">Hobbies</h2>
+      <ul>
+            <li><i class="fa fa-plane" aria-hidden="true"></i>Travel</li>
+            <li><i class="fa fa-car" aria-hidden="true"></i>Cars</li>
+            <li><i class="fa fa-gamepad" aria-hidden="true"></i>Gaming</li>
+            <li><i class="fa fa-music" aria-hidden="true"></i>Music</li>
+            <li><i class="fa fa-road" aria-hidden="true"></i>Road</li>
+                                
+      </ul>      
+    </div>
+</body>
+</html>
